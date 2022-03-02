d95406437167408aae140b86feed56eb62b4ef0f
Yersel Hurtado <yersel500@gmail.com>
2022-03-02 12:48:05 -0500

    Add js score

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const inputName = document.querySelector('.input-name');
+const inputScore = document.querySelector('.input-score');
+const submitBtn = document.querySelector('.submit');
+const theForm = document.querySelector('.the-form');
+const theContainer = document.querySelector('.container-name');
 
-  // Lodash, now improted by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
 
-  return element;
-}
 
-document.body.appendChild(component());
+theForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  // action when submit the form
+  let myName = inputName.value;
+  let myScore = inputScore.value;
+
+  if (myName !== '' && myScore !== '') {
+    const scoreContainer = document.createElement('div');
+    scoreContainer.classList.add('unit-score');
+    scoreContainer.innerHTML = `${myName}: ${myScore}`;
+    theContainer.appendChild(scoreContainer);
+  }
+})
+
+
