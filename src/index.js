78614daf32e66bd3ba1a86238ad94e0d051c2c93
Yersel Hurtado <yersel500@gmail.com>
2022-03-03 12:22:03 -0500

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import './style.css';
 import {
-  inputName, inputScore, theForm, theContainer, refreshBtn
+  inputName, inputScore, theForm, theContainer, refreshBtn,
 } from './modules/selectors.js';
-import {postData, showData} from './modules/post-get.js';
+import { postData, showData } from './modules/post-get.js';
 
 theForm.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -11,18 +11,16 @@ theForm.addEventListener('submit', (e) => {
   const myScore = inputScore.value;
 
   if (myName !== '' && myScore !== '') {
-    let info = new FormData(theForm); 
-    postData('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/lMefZ0gh9HW9MoCXepfi/scores/', {user: info.get('user'), score: info.get('score') })
-    .then(data => {
-      console.log(data);
-    });
+    const info = new FormData(theForm);
+    postData('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/lMefZ0gh9HW9MoCXepfi/scores/', { user: info.get('user'), score: info.get('score') })
+      .then((data) => {
+        console.log(data);
+      });
   }
   inputName.value = '';
   inputScore.value = '';
 });
 
-
 refreshBtn.addEventListener('click', () => {
   showData(theContainer);
-})
-
+});
--- src/modules/post-get.js
+++ src/modules/post-get.js
@@ -5,32 +5,30 @@ export async function postData(url = '', data = {}) {
     cache: 'no-cache',
     credentials: 'same-origin',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
-    redirect: 'follow', 
-    referrerPolicy: 'no-referrer', 
-    body: JSON.stringify(data)
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
+    body: JSON.stringify(data),
   });
   return response.json();
 }
 
 export async function getData() {
-  let response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/lMefZ0gh9HW9MoCXepfi/scores/');
-  let data = await response.json();
+  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/lMefZ0gh9HW9MoCXepfi/scores/');
+  const data = await response.json();
   return data;
 }
 
 export async function showData(container) {
-while(container.firstChild) {
-  container.removeChild(container.firstChild);
-}
-let { result } = await getData();
-result.forEach(gamer => {
-
-  const scoreContainer = document.createElement('div');
-  scoreContainer.classList.add('unit-score');
-  scoreContainer.innerHTML = `${gamer.user}: ${gamer.score}`;
-  container.appendChild(scoreContainer)
-});
-
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+  const { result } = await getData();
+  result.forEach((gamer) => {
+    const scoreContainer = document.createElement('div');
+    scoreContainer.classList.add('unit-score');
+    scoreContainer.innerHTML = `${gamer.user}: ${gamer.score}`;
+    container.appendChild(scoreContainer);
+  });
 }
